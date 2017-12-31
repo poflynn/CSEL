1c6967b6eb7bf040fc63756024f65db7ea509ac4
Paul O'Flynn <paul.oflynn@veeva.com>
2017-12-30 16:09:10 -0800

    Minor Updates

--- install.sh
+++ install.sh
@@ -10,8 +10,10 @@ chmod 777 /usr/local/bin/csel_SCORING_ENGINE_DO_NOT_TOUCH #Make it executable
 
 #Check for crontab entry, add it if it doesn't exist
 echo -e 'DONE\nAdding crontab entry...'
+touch /var/log/csel.log
+chmod 777 /var/log/csel.log
 if [[ $(crontab -l -u root | grep csel) ]] ; then :; else
-  (crontab -l -u root ; echo "*/2 * * * * /usr/local/bin/csel_SCORING_ENGINE_DO_NOT_TOUCH")| crontab -
+  (crontab -l -u root ; echo "*/2 * * * * /usr/local/bin/csel_SCORING_ENGINE_DO_NOT_TOUCH 2&1>/var/log/csel.log")| crontab -
 fi
 
 #Check for CYBER folder, create if it doesn't exist
