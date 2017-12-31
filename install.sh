0e0189f18c9b78d461878eb481d5ff0b588481e1
Paul O'Flynn <paul.oflynn@veeva.com>
2017-12-31 09:42:19 -0800

    Changed scoring refresh to 1m

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ echo -e 'DONE\nAdding crontab entry...'
 touch /var/log/csel.log
 chmod 777 /var/log/csel.log
 if [[ $(crontab -l -u root | grep csel) ]] ; then :; else
-  (crontab -l -u root ; echo "*/2 * * * * /usr/local/bin/csel_SCORING_ENGINE_DO_NOT_TOUCH 2&1>/var/log/csel.log")| crontab -
+  (crontab -l -u root ; echo "*/1 * * * * /usr/local/bin/csel_SCORING_ENGINE_DO_NOT_TOUCH 2&1>/var/log/csel.log")| crontab -
 fi
 
 #Check for CYBER folder, create if it doesn't exist
